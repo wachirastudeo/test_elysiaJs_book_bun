baba4b8b08c242406141c2314971410a6d163f7f
wachira <wachirastudeo@gmail.com>
2024-12-10 17:47:50 +0700

    use getbook

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { Elysia } from "elysia";
-import { getBooks,createBook } from "./model";
+import { getBooks,createBook,getBook} from "./model";
 const app = new Elysia()
 
 app.get('/books', () => {
   return getBooks()})
+  
+app.get('/book/:id', ({params}) => {
+  return getBook(parseInt(params.id))
+  })
 
 app.post('/book', ({ body,set }) => {
- 
+  const bookBody:any = body
   const response = createBook({
-    name: body.name,
-    author: body.author,  
-    price: body.price
+    name: bookBody.name,
+    author: bookBody.author,  
+    price: bookBody.price
   })
   if(response.status === 'error') 
     {
